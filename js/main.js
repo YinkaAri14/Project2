cd413437fca5b077c1a765c82c82b39b62d3889b
YinkaAri14 <aariwa2@lsu.edu>
2024-10-10 10:52:36 -0500

    mobile phone html js css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 // JS scripts placed here
+
+const button = document.getElementById('submit');
+
+button.addEventListener("click", (ev) => {
+    // Prevent the default action of the event
+    ev.preventDefault();
+
+    // declare variables for use
+    const body = document.querySelector('body');
+    const nameOutput = document.getElementById('form-name');
+    console.log(nameOutput);
+
+
+    // Get the value of the input field
+    const nameInput = document.getElementById('name');
+    const nameValue = nameInput.value;
+    console.log('nameInput:', nameInput);
+    console.log('nameValue:', nameValue);
+    nameOutput.innerHTML = nameValue;
+    // alert('You clicked me!, ' + nameValue);
+
+    // Perform action: add class to body element
+    body.classList.add('form-submitted');
+
+    window.scrollTo(0, 0);
+});
+
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
